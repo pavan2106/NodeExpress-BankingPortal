742a7678c1c0b6d4b6bad430a0780ed0dbd40b56
Pavan <pavan2106@gmail.com>
2021-10-13 14:45:13 +0530

    Committing module 2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 //const { response } = require('express');
 //const request = require('request');
 const app = express();
-const port = process.env.PORT || 3000;
+const port =  3000;
+// const { accounts} = require('./json/accounts.json');
 
 // app.get('/', funtion(request, response){
 
@@ -15,17 +16,38 @@ const port = process.env.PORT || 3000;
 // });
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, '/public')));
-app.get('/index', function (req, res) {
-    res.render('index.ejs', {
-        title: "Index"
-    });
-});
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+// const writeJSON = () => {
+//     let accountsJSON = JSON.stringify(accounts, null, 4)
+//     fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+// }
 app.get('/', function (req, res) {
     res.render('index', {
-        title: "Index"
+        title: "Account Summary",
+        accounts: accounts 
     });
+});
+app.get('/profile', function(req, res){
+     res.render('profile', { user: users[0] })
+});
+
+app.get('/credit', function(req, res){
+    res.render('account', { account: accounts.credit })
+});
+
+app.get('/savings', function(req, res){
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', function(req, res){ 
+    res.render('account', { account: accounts.checking })
 });
 
 app.listen(port, function() {
